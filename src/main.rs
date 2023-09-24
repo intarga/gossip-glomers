5f71e771c78567d44854a3e1ba28998c719c401d
Ingrid <git@ingrids.email>
2023-09-24 21:39:17 +0200

    solve 3a

--- src/main.rs
+++ src/main.rs
@@ -20,6 +20,18 @@ enum ExtraFields {
     GenerateOk {
         id: String,
     },
+    Broadcast {
+        message: usize,
+    },
+    BroadcastOk,
+    Read,
+    ReadOk {
+        messages: Vec<usize>,
+    },
+    Topology {
+        topology: HashMap<String, Vec<String>>,
+    },
+    TopologyOk,
 }
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
@@ -55,6 +67,7 @@ impl MsgIdGen {
 struct NodeData {
     node_id: Option<String>,
     node_ids: Option<Vec<String>>,
+    messages: Vec<usize>,
     topology: Option<HashMap<String, Vec<String>>>,
 }
 
@@ -63,8 +76,25 @@ impl NodeData {
         Self {
             node_id: None,
             node_ids: None,
+            messages: Vec::new(),
             topology: None,
         }
+    }
+
+    fn neighbours_from_topology(&self) -> Option<Vec<String>> {
+        Some(
+            self.topology
+                .clone()?
+                .get(&(self.node_id.clone()?))?
+                .clone(),
+        )
+    }
+
+    pub fn get_neighbours(&self) -> Option<Vec<String>> {
+        Some(
+            self.neighbours_from_topology()
+                .unwrap_or(self.node_ids.clone()?),
+        )
     }
 }
 
@@ -73,7 +103,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let in_handle = stdin.lock();
     let mut de = serde_json::Deserializer::from_reader(in_handle);
 
-    let mut NodeData = NodeData::new();
+    let mut node_data = NodeData::new();
 
     let mut msg_id_gen = MsgIdGen::new();
 
@@ -87,18 +117,48 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                 node_id,
                 node_ids: _,
             } => {
-                NodeData.node_id = Some(node_id);
+                node_data.node_id = Some(node_id);
                 Some(ExtraFields::InitOk)
             }
+            ExtraFields::InitOk => None,
             ExtraFields::Echo { echo } => Some(ExtraFields::EchoOk { echo }),
+            ExtraFields::EchoOk { echo: _ } => None,
             ExtraFields::Generate => Some(ExtraFields::GenerateOk {
                 id: format!(
                     "{}-{}",
-                    NodeData.node_id.clone().unwrap_or_default(),
+                    node_data.node_id.clone().unwrap_or_default(),
                     msg_id
                 ),
             }),
-            _ => panic!(),
+            ExtraFields::GenerateOk { id: _ } => None,
+            ExtraFields::Broadcast { message } => {
+                node_data.messages.push(message);
+                if let Some(neighbours) = node_data.get_neighbours() {
+                    for neighbour in neighbours {
+                        let fwd_msg = Msg {
+                            src: node_data.node_id.clone().unwrap(),
+                            dst: neighbour,
+                            body: MsgBody {
+                                msg_id: msg_id_gen.gen(),
+                                in_reply_to: None,
+                                extra: ExtraFields::Broadcast { message },
+                            },
+                        };
+                        println!("{}", serde_json::to_string(&fwd_msg)?);
+                    }
+                }
+                Some(ExtraFields::BroadcastOk)
+            }
+            ExtraFields::BroadcastOk => None,
+            ExtraFields::Read => Some(ExtraFields::ReadOk {
+                messages: node_data.messages.clone(),
+            }),
+            ExtraFields::ReadOk { messages: _ } => None,
+            ExtraFields::Topology { topology } => {
+                node_data.topology = Some(topology);
+                Some(ExtraFields::TopologyOk)
+            }
+            ExtraFields::TopologyOk => None,
         };
 
         if let Some(inner) = ok_extra {
