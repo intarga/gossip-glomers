421c6a8715da9d543013a4340c28e0ea0a9ce5a3
Ingrid <git@ingrids.email>
2023-09-24 20:14:46 +0200

    get smarter with the serde structure

--- src/main.rs
+++ src/main.rs
@@ -4,25 +4,27 @@ use std::io;
 #[derive(Debug, Clone, Serialize, Deserialize)]
 #[serde(tag = "type")]
 #[serde(rename_all = "snake_case")]
-enum MsgBody {
+enum ExtraFields {
     Init {
-        msg_id: usize,
         node_id: String,
         node_ids: Vec<String>,
     },
-    InitOk {
-        msg_id: usize,
-        in_reply_to: usize,
-    },
+    InitOk,
     Echo {
-        msg_id: usize,
         echo: String,
     },
     EchoOk {
-        msg_id: usize,
-        in_reply_to: usize,
         echo: String,
     },
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+struct MsgBody {
+    msg_id: usize,
+    in_reply_to: Option<usize>,
+
+    #[serde(flatten)]
+    extra: ExtraFields,
 }
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
@@ -54,35 +56,26 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut msg_id_gen = MsgIdGen::new();
 
     loop {
-        eprintln!("pre des");
         let in_msg = Msg::deserialize(&mut de)?;
-        eprintln!("des ok");
 
-        let out_body = match in_msg.body {
-            MsgBody::Init {
-                msg_id,
+        let out_inner = match in_msg.body.extra {
+            ExtraFields::Init {
                 node_id: _,
                 node_ids: _,
-            } => MsgBody::InitOk {
-                msg_id: msg_id_gen.gen(),
-                in_reply_to: msg_id,
-            },
-            MsgBody::Echo { msg_id, echo } => MsgBody::EchoOk {
-                msg_id: msg_id_gen.gen(),
-                in_reply_to: msg_id,
-                echo,
-            },
+            } => ExtraFields::InitOk,
+            ExtraFields::Echo { echo } => ExtraFields::EchoOk { echo },
             _ => panic!(),
         };
         let out_msg = Msg {
             src: in_msg.dst,
             dst: in_msg.src,
-            body: out_body,
+            body: MsgBody {
+                msg_id: msg_id_gen.gen(),
+                in_reply_to: Some(in_msg.body.msg_id),
+                extra: out_inner,
+            },
         };
-        eprintln!("reply constructed");
 
         println!("{}", serde_json::to_string(&out_msg)?);
-
-        eprintln!("reply sent");
     }
 }
