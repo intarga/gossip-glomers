c4ea1533d9575bdb8a425fefbc59992292be3a2f
Ingrid <git@ingrids.email>
2023-09-24 19:47:10 +0200

    implement init

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,63 @@
-fn main() {
-    println!("Hello, world!");
+use serde::{Deserialize, Serialize};
+use std::io;
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+#[serde(tag = "type")]
+#[serde(rename_all = "snake_case")]
+enum MsgBody {
+    Init {
+        msg_id: usize,
+        node_id: String,
+        node_ids: Vec<String>,
+    },
+    InitOk {
+        msg_id: usize,
+        in_reply_to: usize,
+    },
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+struct Msg {
+    src: String,
+    #[serde(rename = "dest")]
+    dst: String,
+    body: MsgBody,
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let stdin = io::stdin();
+    let in_handle = stdin.lock();
+    let mut de = serde_json::Deserializer::from_reader(in_handle);
+
+    let mut curr_id = 1;
+
+    loop {
+        eprintln!("pre des");
+        let in_msg = Msg::deserialize(&mut de)?;
+        eprintln!("des ok");
+
+        let out_body = match in_msg.body {
+            MsgBody::Init {
+                msg_id,
+                node_id: _,
+                node_ids: _,
+            } => MsgBody::InitOk {
+                msg_id: curr_id,
+                in_reply_to: msg_id,
+            },
+            _ => panic!(),
+        };
+        let out_msg = Msg {
+            src: in_msg.dst,
+            dst: in_msg.src,
+            body: out_body,
+        };
+        eprintln!("reply constructed");
+
+        curr_id += 1;
+
+        println!("{}", serde_json::to_string(&out_msg)?);
+
+        eprintln!("reply sent");
+    }
 }
